7b60afb3da88ac2e0c53408c7a69ed422f154b8f
Nooshin bakhtiari <nooshindev@gmail.com>
2023-04-15 00:42:13 +0430

    add style to share icon on mobile device

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-import "./style.css";
+'use strict'
+// selecting the elements
+const btnShare = document.querySelector('.button-share-icon')
+
+// console.log(btnShare)
+
+btnShare.addEventListener('click', function () {
+  
+   
+})
