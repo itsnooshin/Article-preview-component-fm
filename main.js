036237e4c7c1b4e7da1feface146b35965e2e7f2
Nooshin bakhtiari <nooshindev@gmail.com>
2023-04-22 00:11:34 +0430

    add functionality to mobile device

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 'use strict'
 // selecting the elements
 const btnShare = document.querySelector('.button-share-icon')
+const profile = document.querySelector('.information-footer')
+const footer = document.querySelector('.container-footer')
+const information = document.querySelector('.information-footer')
+const svgIcon = document.querySelector('.img-svg path')
+const hidden = document.querySelector('.hidden')
+const hidde = document.querySelector('.conatiner__hidd')
 
-// console.log(btnShare)
 
 btnShare.addEventListener('click', function () {
-  
-   
+  footer.style.background = '#48556A'
+  information.style.display = 'none'
+  hidde.style.display = 'block'
+  btnShare.style.background = '#6E8098'
+  svgIcon.style.fill = '#fff'
 })
