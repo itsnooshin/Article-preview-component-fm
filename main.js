fd957650eac8267683e74a33578fdfe0489795c8
Nooshin bakhtiari <nooshindev@gmail.com>
2023-04-22 21:58:08 +0430

    add style to share link to project

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const svgIcon = document.querySelector('.img-svg path')
 const hidden = document.querySelector('.hidden')
 const hidde = document.querySelector('.conatiner__hidd')
 
-
+// const body = (document.body.style.minWidth = '300px')
+hidde.style.display = 'block'
 btnShare.addEventListener('click', function () {
   footer.style.background = '#48556A'
   information.style.display = 'none'
