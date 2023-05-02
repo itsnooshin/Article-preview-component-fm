34a616e97b826bcdf20ccb058644a686d60b013d
Nooshin bakhtiari <nooshindev@gmail.com>
2023-05-02 17:19:12 +0430

    delete the extra code on the files

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 'use strict'
-
 const btnShare = document.querySelector('.btn-share-icon')
-const profile = document.querySelector('.information-footer')
-const footer = document.querySelector('.container-footer')
-const information = document.querySelector('.information')
-const svgIcon = document.querySelector('.img-svg path')
-const hiddenContainer = document.querySelector('.conatiner__hidd')
 const social = document.querySelector('.socials')
-const infoel = document.querySelector('.container-info')
-const btnHide = document.querySelector('.btn-share-hide')
+
 btnShare.addEventListener('click', function (e) {
   e.preventDefault()
   social.classList.toggle('show')
