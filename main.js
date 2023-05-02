d7285a28221a9f04c2ee7d9c330f9fe865146562
Nooshin bakhtiari <nooshindev@gmail.com>
2023-05-02 17:25:27 +0430

    fix bug on javascript file

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 'use strict'
+
 const btnShare = document.querySelector('.btn-share-icon')
 const social = document.querySelector('.socials')
-
+const btnHide = document.querySelector('.btn-share-hide')
 btnShare.addEventListener('click', function (e) {
   e.preventDefault()
   social.classList.toggle('show')
