391e029465c1033f1604a0a0c744e616f9ac7071
Nooshin bakhtiari <nooshindev@gmail.com>
2023-05-02 16:55:00 +0430

    fix the desktop view and the mobile view

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 'use strict'
 
-const btnShare = document.querySelector('.btn-share')
+const btnShare = document.querySelector('.btn-share-icon')
 const profile = document.querySelector('.information-footer')
 const footer = document.querySelector('.container-footer')
-const information = document.querySelector('.information-footer')
+const information = document.querySelector('.information')
 const svgIcon = document.querySelector('.img-svg path')
 const hiddenContainer = document.querySelector('.conatiner__hidd')
 const social = document.querySelector('.socials')
-
-
+const infoel = document.querySelector('.container-info')
+const btnHide = document.querySelector('.btn-share-hide')
 btnShare.addEventListener('click', function (e) {
   e.preventDefault()
   social.classList.toggle('show')
-  information.classList.toggle('hide')
-  footer.classList.toggle('show')
+  social.classList.toggle('hide')
+})
 
-
+btnHide.addEventListener('click', function (e) {
+  social.classList.toggle('show')
+  btnShare.classList.toggle('.hide')
 })
